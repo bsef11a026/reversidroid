19b8c090486d35cca55ab36b37a5ed519fff6e05
fcherchi <fcherchi@792d33ce-ee65-11de-83d8-6d5adf01d787>
2009-12-26 20:43:11 +0000

    added score and settings menu

--- trunk/org.cherchi.reversi/org.cherchi.reversi.logic/src/main/java/org/cherchi/reversi/logic/internal/GameFacadeImpl.java
+++ trunk/org.cherchi.reversi/org.cherchi.reversi.logic/src/main/java/org/cherchi/reversi/logic/internal/GameFacadeImpl.java
@@ -1,29 +1,33 @@
 package org.cherchi.reversi.logic.internal;
 
+import org.cherchi.reversi.logic.GameEventsListener;
 import org.cherchi.reversi.logic.GameFacade;
 import org.cherchi.reversi.logic.GameLogic;
 
 /**
  * The implementation of the high level method used in the game
+ * 
  * @author Fernando Cherchi
- *
+ * 
  */
 public class GameFacadeImpl implements GameFacade {
 
 	// /////////////////////// CONSTANTS ///////////////////////////////
-	
-	
 
 	// /////////////////////// PRIVATE FIELDS //////////////////////////
-	
+
 	/**
-	 * access to the game operative
+	 * access to the game operations
 	 */
 	private GameLogic gameLogic;
-	
-	// /////////////////////// PUBLIC METHODS //////////////////////////	
-	
-	
+
+	/**
+	 * This is the object to be notified of the game situation
+	 */
+	private GameEventsListener gameEventsListener;
+
+	// /////////////////////// PUBLIC METHODS //////////////////////////
+
 	/**
 	 * Starts a new game
 	 */
@@ -38,22 +42,24 @@ public class GameFacadeImpl implements GameFacade {
 	 */
 	@Override
 	public void set(int player, int col, int row) {
-		
+
 		if (this.gameLogic.canSet(player, col, row)) {
 			this.gameLogic.setChip(player, col, row);
 			this.gameLogic.conquerPosition(player, col, row);
 			this.togglePlayer();
+			this.notifyChanges();
 		}
 	}
-	
+
 	/**
 	 * Gets the current allowed positions for current Player
 	 */
 	@Override
 	public int[][] getAllowedPositionsForPlayer() {
-		return this.gameLogic.getAllowedPositionsForPlayer(this.getCurrentPlayer());
+		return this.gameLogic.getAllowedPositionsForPlayer(this
+				.getCurrentPlayer());
 	}
-	
+
 	/**
 	 * Gets the current matrix of the game
 	 */
@@ -62,32 +68,52 @@ public class GameFacadeImpl implements GameFacade {
 		return this.gameLogic.getGameMatrix();
 	}
 
+	/**
+	 * Sets the event listener (is going to be notified when the game situation
+	 * changes)
+	 */
+	@Override
+	public void setGameEventsListener(GameEventsListener listener) {
+		this.gameEventsListener = listener;
+	}
+
 	// /////////////////////// PRIVATE METHODS ///////////////////////
+
+	/**
+	 * Notifies the listener for the changes occured in the game
+	 */
+	private void notifyChanges() {
+		if (this.gameEventsListener != null) {
+			this.gameEventsListener.onScoreChanged(this.gameLogic
+					.getCounterForPlayer(PLAYER_ONE), this.gameLogic
+					.getCounterForPlayer(PLAYER_TWO));
+		}
+	}
+
 	/**
 	 * Changes the player
 	 */
 	private void togglePlayer() {
-		
-		int current = this.gameLogic.getCurrentPlayer();
-		//if the current player can play (has at least one place to put the chip)
-		if (! this.gameLogic.isBlockedPlayer(current)) {
 
-			//just toggles
+		int current = this.gameLogic.getCurrentPlayer();
+		// if the current player can play (has at least one place to put the
+		// chip)
+		if (!this.gameLogic.isBlockedPlayer(current)) {
+
+			// just toggles
 			if (current == GameLogic.PLAYER_ONE) {
-				this.gameLogic.setCurrentPlayer (GameLogic.PLAYER_TWO);
+				this.gameLogic.setCurrentPlayer(GameLogic.PLAYER_TWO);
 			} else {
-				this.gameLogic.setCurrentPlayer (GameLogic.PLAYER_ONE);
+				this.gameLogic.setCurrentPlayer(GameLogic.PLAYER_ONE);
 			}
 		}
 	}
 
-	
-	
 	// /////////////////////// ACCESSORS //////////////////////////
-	
 
 	/**
-	 * @param gameLogic the gameLogic to set
+	 * @param gameLogic
+	 *            the gameLogic to set
 	 */
 	public void setGameLogic(GameLogic gameLogic) {
 		this.gameLogic = gameLogic;
@@ -108,8 +134,11 @@ public class GameFacadeImpl implements GameFacade {
 		return this.gameLogic.getCurrentPlayer();
 	}
 
+	/**
+	 * Gets the score for the given player
+	 */
 	@Override
-	public int getCounterForPlayer(int player) {
+	public int getScoreForPlayer(int player) {
 		return this.gameLogic.getCounterForPlayer(player);
 	}
 
